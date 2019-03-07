6056fd194d2caf3c7d2c522292f8121c6a09d398
Tian du Toit <tian@hmsnz.com>
2019-03-07 16:01:54 +1300

    initial Board component setup

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,47 @@
+// import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 
 export class Board extends Component {
   static propTypes = {
     prop: PropTypes
   }
+  constructor(props) {
+    super(props)
+    this.state = {
+      results: [],
+      bank: 10000
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(){
+    let max = 36
+    let min = 0
+    let result = Math.floor(Math.random()*(max-min+1)+min);
+  }
 
   render() {
-    return <div />
+    return <div>
+      <button></button>
+      <div>
+        <div>
+          result:
+
+        </div>
+        <div>
+          bank:
+        </div>
+      </div>
+    </div>
   }
 }
 
-const mapStateToProps = state => ({})
+// const mapStateToProps = state => ({})
 
-const mapDispatchToProps = {}
+// const mapDispatchToProps = {}
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Board)
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(Board)
