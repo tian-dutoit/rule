8491b14afb0a66d355d8c2cadc80e1458546da27
Tian du Toit <tian@hmsnz.com>
2019-03-07 16:49:53 +1300

    fix array.push functions

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -18,21 +18,18 @@ export class Board extends Component {
     const max = 36
     const min = 0
     const result = Number(Math.floor(Math.random() * (max - min + 1) + min))
-    const newResult = this.state.results.slice()
+    const newResult = this.state.results.slice(0)
     newResult.push(result)
-    const newMovingBank = this.state.movingBank.slice()
-    newMovingBank.push(
+    const newBank =
       result < 25
-        ? this.state.movingBank - this.state.bet
-        : this.state.movingBank + this.state.bet * 3
-    )
+        ? this.state.bank - this.state.bet
+        : this.state.bank + this.state.bet * 3
+    const newMovingBank = this.state.movingBank.slice(0)
+    newMovingBank.push(newBank)
     this.setState({
       result: result,
       results: newResult,
-      bank:
-        result < 25
-          ? this.state.bank - this.state.bet
-          : this.state.bank + this.state.bet * 3,
+      bank: newBank,
       movingBank: newMovingBank,
       bet: result < 25 ? 5 : this.state.bet * 2
     })
@@ -52,12 +49,12 @@ export class Board extends Component {
             </div>
           </div>
           <div className="column-container">
-            bank:
-            <div>
+            bank: {this.state.bank}
+            {/* <div>
               {this.state.movingBank.map((bank, idx) => {
                 return <div key={idx}>{bank}</div>
               })}
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
